ae33885dc08e1907035de6c55451fce57bd6ec66
mohamed <baghaoui@hotmail.com>
2022-10-09 15:45:27 +0200

    chargement des urls d'une page de 20 livres definition d'une fonction book_info qui est utilisée dans scap category

--- scrap_one_book.py
+++ scrap_one_book.py
@@ -4,118 +4,36 @@ import csv
 
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
-response = requests.get(url)
 
-#print(url)
+def book_info(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    all_infos = soup.find_all("td")
+    infos = []
+    for info in all_infos:
+        infos.append(info.string)
+    # avoir toutes les info pour un livre
+    product_page_url = url
+    upc = infos[0]
+    title = soup.find('article').h1.text
+    price_excluding_tax = infos[2]
+    price_including_tax = infos[3]
+    number_available = infos[5]
+    review_rating = infos[6]
+    product_description = soup.find('article').find('p', recursive=False).text
+    category = soup.find_all("a")[3].text
+    main_url = 'http://books.toscrape.com/'
+    image_url = soup.find('img')['src'].replace('../../', main_url)
+    return product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url
 
-soup = BeautifulSoup(response.text, 'html.parser')
-
-all_infos = soup.find_all("td")
-infos = []
-for info in all_infos:
-	infos.append(info.string)
-#print(infos) #avoir toutes les info pour un livre
-UPC = infos[0]
-#print(UPC)
-title = soup.find('article').h1.text
-price_excluding_tax = infos[2]
-#print(price_excluding_tax)
-price_including_tax = infos[3]
-#print(price_including_tax)
-number_available = infos[5]
-#print(number_available)
-review_rating = infos[6]
-#print(review_rating)
-product_description = soup.find('article').find('p', recursive = False).text #recursive=false renvoir le premier 'child' qui s'appel 'p' dans article
-#print(product_description)
-category = soup.find_all("a")[3].text #sortir le 4eme "a" trouvé
-#print(category)
-main_url = 'http://books.toscrape.com/'
-image_url = soup.find('img')['src'].replace('../../', main_url)
-#print(image_url)
 
 # creation du fichier CSV avec toutes les info en entete
-en_tete = ['product_page_url', 'UPC', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+en_tete = ['product_page_url', 'UPC', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available',
+           'product_description', 'category', 'review_rating', 'image_url']
 with open('book_infos.csv', 'w') as csv_file:
-	writer = csv.writer(csv_file, delimiter=',')
-	writer.writerow(en_tete)
-	writer.writerow([url, UPC, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
-
-#2 def des  fonctions qui permettent de recuperer automatiquement les elements ci dessus
-
-
-#3 recuperer tout les url d'une categorie , S'il y a un lien "next" sur la page, le script doit visiter ce lien et extraire
-#les informations de cette page.
-
-
-
-
-
-'''
-#Grabbing the link of each book
-for link in article.find_all('a',href = True):
-    url = link['href']
-    Books_url.append('https://books.toscrape.com/catalogue/' + url)
-#print(Books_url)  
-
-
-
-
-
-
-
-
-
-
-# (à utiliser plus tard ) zip permet d'itérer sur plusieure liste listes à la fois
-#for url, UPC, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url in zip(url,UPC, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url):
-
-""""
-
-
-
-image_url = infos[]
-
-
-for tr in trs:
-	a = tr.find('td.str')
-	info = str('a')
-print(info)
-
-Objectif du projet : suivre les prix des livre sur : Books to scrape
-
-1ere étape, choisir la page d'un seul produit et
-créer un fichier csv qui a comme en-tête:
-# product_page_url
-# universal_ product_code (upc)
-# title
-# price_including_tax
-# price_excluding_tax
-# number_available
-# product_description
-# category
-# review_rating
-# image_url
-
-# j'importe requests et bs4
-#universal_ product_code = soup.find_all("UPC", class_="table table-striped")
-#print(niversal_ product_code)
-
-
-2-faire la meme chose pour une catégorie de livre 
-
-remarque : plusieurs page dans une mm cathégorie 
-
-3-appliquer le meme travail sur l'ensemble du site 
-
-ecrire un fichier csv pour chaque catégorie 
-
-4- téléchanrger et enregisrter le fichier image de chaque page produit consultée
-
-5-enregistrer le travails sur un GitHub + commits avec des messages de commits (pour chaque etape par exemple)
-
-6- enregisrter un fichier requirements.txt (sans l'environement retuel et sans les csv)
-7- ecrire un README.md
-
-Python 
-'''
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(en_tete)
+    csv_data = book_info(url)
+    writer.writerow(
+        [csv_data[0], csv_data[1], csv_data[2], csv_data[3], csv_data[4], csv_data[5], csv_data[6], csv_data[7],
+         csv_data[8], csv_data[9]])
--- scrap_one_category.py
+++ scrap_one_category.py
@@ -1,25 +1,35 @@
-#on choisi la category sequentiel-art
-
+# on choisi la category sequentiel-art
+import csv
 import requests
 from bs4 import BeautifulSoup
+from scrap_one_book import book_info
 
 url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-#recuperer les lien d'une page
-h3s = soup.findAll("h3")
+# recuperer les lien d'une page
 url_book_list = []
 page_url = []
 urls_category = []
+urls = []
+
+#extraire les liens d'une seule page
+h3s = soup.findAll("h3")
 for h3 in h3s:
     link = h3.find("a")["href"].replace("../../..", "http://books.toscrape.com/catalogue")
     url_book_list.append(link)
 print(url_book_list)
 print(len(url_book_list))
-#cas de plusieur pages
-if len(url_book_list) == 20: #on cherche "next"
-    next_page = soup.find('ul', 'pager').find('li', 'next')
-    page_number = 1
-    while next_page:
 
+with open('book_infos.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    en_tete = ['product_page_url', 'UPC', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available',
+               'product_description', 'category', 'review_rating', 'image_url']
+    writer.writerow(en_tete)
+    for urls in url_book_list:
+        csv_books_data = book_info(urls)
+        writer.writerow(
+            [csv_books_data[0], csv_books_data[1], csv_books_data[2], csv_books_data[3], csv_books_data[4],
+             csv_books_data[5], csv_books_data[6], csv_books_data[7],
+             csv_books_data[8], csv_books_data[9]])
